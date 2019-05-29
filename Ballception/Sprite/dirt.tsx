07a60ec11527c1117f5edcc52800cd569c04f746
ArfreeX <blicharz.mat@gmail.com>
2019-05-30 01:27:15 +0200

    More assets added: Map, Animations, Sound

--- Ballception/Sprite/dirt.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="dirt" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="dirt.jpg" width="345" height="345"/>
-</tileset>
